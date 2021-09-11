3450a49d3c305de9cce8f1b2510a2f01e53b3ec8
Carlos Iván Ochoa Gómez <81680537+CIOG2@users.noreply.github.com>
2021-09-11 17:18:52 -0600

    Archivos base para comenzar con el proyecto

---
+++ JS/PreguntasGenerales/historia.js
@@ -0,0 +1,53 @@
+// const Historia = {
+//     Faciles: {
+//         1: {
+//             pregunta: '',
+//             opcion1: '',
+//             opcion2: '',
+//             opcion3: '',
+//             opcion4: '',
+//             respuesta: '',
+//             imagen: '',
+//             Fuente: '',
+//         },
+//     Medianas: {
+//         1: {
+//             pregunta: '',
+//             opcion1: '',
+//             opcion2: '',
+//             opcion3: '',
+//             opcion4: '',
+//             respuesta: '',
+//             imagen: '',
+//         },
+//         2: {
+//             pregunta: '',
+//             opcion1: '',
+//             opcion2: '',
+//             opcion3: '',
+//             opcion4: '',
+//             respuesta: '',
+//             imagen: '',
+//         },
+//     },
+//     Dificiles: {
+//         1: {
+//             pregunta: '',
+//             opcion1: '',
+//             opcion2: '',
+//             opcion3: '',
+//             opcion4: '',
+//             respuesta: '',
+//             imagen: '',
+//         },
+//         2: {
+//             pregunta: '',
+//             opcion1: '',
+//             opcion2: '',
+//             opcion3: '',
+//             opcion4: '',
+//             respuesta: '',
+//             imagen: '',
+//         },
+//     },
+// }
