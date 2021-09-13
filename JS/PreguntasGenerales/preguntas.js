4bd89cb49d35379d836c6670040c2a60ffbfb21f
Carlos Iván Ochoa Gómez <81680537+CIOG2@users.noreply.github.com>
2021-09-13 15:47:12 -0600

    Cree la clase preguntas para tener mas ordenal generar las preguntas de cada tema

---
+++ JS/PreguntasGenerales/preguntas.js
@@ -0,0 +1,23 @@
+class Preguntas{
+    constructor({
+        pregunta,
+        opcion1,
+        opcion2,
+        opcion3,
+        opcion4,
+        respuesta,
+        imagen = "none",
+        imagenRespuesta  = "none", 
+    }){
+        this.pregunta = pregunta;
+        this.opcion1 = opcion1;
+        this.opcion2 = opcion2;
+        this.opcion3 = opcion3;
+        this.opcion4 = opcion4;
+        this.respuesta = respuesta;
+        this.imagen = imagen;    
+        this.imagenRespuesta = imagenRespuesta;
+    }
+}
+
+export default Preguntas;
---
+++ JS/index.js
@@ -0,0 +1,4 @@
+import Geografia from "./PreguntasGenerales/geografia.js";
+
+
+console.log(Geografia[0].pregunta);
