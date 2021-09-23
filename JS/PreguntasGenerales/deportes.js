85c1b7c81d21984078b2913c4c1e14097a89bf0c
Manuel Alejandro <alejandro.lynk62@gmail.com>
2021-09-22 21:22:04 -0600

    Agregue una pregunta, ademas la vez comente la sintaxis de las preguntas (comentadas)

---
+++ JS/PreguntasGenerales/deportes.js
@@ -0,0 +1,35 @@
+import Preguntas from "./preguntas.js";
+const Deportes  = [];
+
+//Funcion para autoSumar y evitar conflictos al generar preguntas
+let NUM = 0;
+
+
+/*
+SINTAXIS DE LAS PREGUNTAS
+    Deportes[Numero()] = new Preguntas({
+        pregunta: "",
+        opcion1: "",
+        opcion2: "",
+        opcion3: "",
+        opcion4: "",
+        respuesta: "",
+        imagen: "",
+        imagenRespuesta: "url", 
+    }); 
+*/
+
+const Numero = () => NUM++;
+
+Deportes[Numero()] = new Preguntas({
+    pregunta: "¿Quien es el hombre mas veloz de la historia?",
+    opcion1: "Lamont Marcell",
+    opcion2: "Johnny Weissmuller",
+    opcion3: "Justin Gatlin",
+    opcion4: "Usain Bolt",
+    respuesta: "Usain Bolt",
+    imagenRespuesta: "https://as01.epimg.net/masdeporte/imagenes/2019/08/16/atletismo/1565957973_435409_1565958289_noticia_normal.jpg", 
+});
+
+
+export default Deportes;
